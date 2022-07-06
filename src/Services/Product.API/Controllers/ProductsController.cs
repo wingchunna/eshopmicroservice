deb4c7cc072eeb32663b5fdf1f4ded58084950ac
wingchunna <linktink123@gmail.com>
2022-07-06 14:40:59 +0700

    Product CRUD

--- src/Services/Product.API/Controllers/ProductsController.cs
+++ src/Services/Product.API/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Product.API.Entities;
 using Product.API.Persistence;
 using Product.API.Repositories.Interfaces;
+using Shared.DTOs.Product;
 using System.ComponentModel.DataAnnotations;
 
 namespace Product.API.Controllers
@@ -14,15 +15,75 @@ namespace Product.API.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly IProductRepository _repository;
-        public ProductsController(IProductRepository repository)
+        private readonly IMapper _mapper;
+        public ProductsController(IProductRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
+
+        #region CRUD
         [HttpGet]
         public async Task<IActionResult> GetProducts()
         {
-            var result = await _repository.GetProducts();
+            var products = await _repository.GetProducts();
+            var result = _mapper.Map<IEnumerable<ProductDto>>(products);
             return Ok(result);
         }
+
+        [HttpGet("{id:long}")]
+        public async Task<IActionResult> GetProduct([Required] long id)
+        {
+            var product = await _repository.GetProduct(id);
+            if (product == null)
+                return NotFound();
+
+            var result = _mapper.Map<ProductDto>(product);
+            return Ok(result);
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductDto productDto)
+        {
+         
+
+            var product = _mapper.Map<CatalogProduct>(productDto);
+            await _repository.CreateProduct(product);
+            await _repository.SaveChangesAsync();
+            var result = _mapper.Map<ProductDto>(product);
+            return Ok(result);
+        }
+
+
+        [HttpDelete("{id:long}")]
+        public async Task<IActionResult> DeleteProduct([Required] long id)
+        {
+            var product = await _repository.GetProduct(id);
+            if (product == null)
+                return NotFound();
+
+            await _repository.DeleteProduct(id);
+            await _repository.SaveChangesAsync();
+            return NoContent();
+        }
+
+        #endregion
+
+        #region Additional Resources
+
+        [HttpGet("get-product-by-no/{productNo}")]
+        public async Task<IActionResult> GetProductByNo([Required] string productNo)
+        {
+            var product = await _repository.GetProductByNo(productNo);
+            if (product == null)
+                return NotFound();
+
+            var result = _mapper.Map<ProductDto>(product);
+            return Ok(result);
+        }
+
+        #endregion
     }
 }
---
+++ src/Services/Product.API/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Infrastructure.Mappings;
+using Product.API.Entities;
+using Shared.DTOs.Product;
+
+namespace Product.API
+{
+    // sử dụng mapper để thay đổi dữ liệu hiển thị. Loại bỏ trường dữ liệu không cần thiết như created_date, updated_date
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CatalogProduct, ProductDto>();
+            CreateMap<CreateProductDto, CatalogProduct>();
+            CreateMap<UpdateProductDto, CatalogProduct>().IgnoreAllNonExisting(); // thêm IgnoreAll để những trường dữ liệu nào ko update thì sẽ bỏ qua và giữ nguyên giá trị chứ ko bị gán null
+
+        }
+    }
+}
